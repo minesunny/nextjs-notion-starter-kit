b9551e6464d1e6ae9d114cddc3485cdc8ea68f29
mine <minesunny@icloud.com>
2023-04-08 01:50:19 +0800

    feat: 完善github action部署服务器

--- next.config.js
+++ next.config.js
@@ -1,22 +1,27 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
-})
-
-module.exports = withBundleAnalyzer({
+const exp = {
   staticPageGenerationTimeout: 300,
   images: {
-    domains: [
-      'www.notion.so',
-      'notion.so',
-      'images.unsplash.com',
-      'pbs.twimg.com',
-      'abs.twimg.com',
-      's3.us-west-2.amazonaws.com',
-      'transitivebullsh.it'
-    ],
+  domains: [
+    'www.notion.so',
+    'notion.so',
+    'images.unsplash.com',
+    'pbs.twimg.com',
+    'abs.twimg.com',
+    's3.us-west-2.amazonaws.com',
+    'transitivebullsh.it'
+  ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
   }
-})
+}
+if (process.env.NODE_ENV === 'production') {
+  module.exports = exp
+} else {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true'
+  })
+  module.exports = withBundleAnalyzer(exp)
+}
+
