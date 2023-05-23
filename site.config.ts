da43f624dc880be23130ac0e375c7b8b4da4a7bc
minesunny <98507458+minesunny@users.noreply.github.com>
2023-05-23 14:22:29 +0800

    Update site.config.ts
    enable redis cache

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'www.minesunny.cn',
+  domain: 'www.minesunny.com',
   author: 'Mine',
 
   // open graph metadata (optional)
@@ -36,7 +36,7 @@ export default siteConfig({
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
   // environment variables. see the readme for more info
-  isRedisEnabled: false,
+  isRedisEnabled: true,
 
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths
